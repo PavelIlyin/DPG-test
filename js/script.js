d426fe02ab692b47e38a13b3eff2b7775cda7d60
Павел Андреевич <71135153+PavelIlyin@users.noreply.github.com>
2023-01-24 01:24:19 +0500

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,11 +1,18 @@
 'use strict'
-
+let inputLabel = document.querySelectorAll('label')
 let input = document.querySelectorAll('.forms__block-input');
+
+
+function clickColor(event) {
+    for (let i in inputLabel) {
+        if (inputLabel[i].className === "active") {
+            inputLabel[i].classList.remove("active")
+        }
+    }
+    event.target.parentNode.classList.add("active")
+
+}
 
 input.forEach(e => {
     addEventListener('click', clickColor)
 });
-
-function clickColor(event) {
-    event.target.parentNode.classList.toggle("active")
-}
